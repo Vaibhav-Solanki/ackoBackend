f34ba25bb0aa7518ec2d6af03f6023771a40e10f
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-30 13:35:03 +0530

    Update server.js

--- src/routes/server.js
+++ src/routes/server.js
@@ -40,7 +40,7 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
     params["ORDER_ID"] = "TEST_" + new Date().getTime();
     params["CUST_ID"] = paymentDetails.customerId;
     params["TXN_AMOUNT"] = paymentDetails.amount;
-    params["CALLBACK_URL"] = `http://localhost:${process.env.PORT}/callback`;
+    params["CALLBACK_URL"] = `https://ackobackend.herokuapp.com/callback`;
     params["EMAIL"] = paymentDetails.customerEmail;
     params["MOBILE_NO"] = paymentDetails.customerPhone;
 
