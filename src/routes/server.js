6b705057ff4c272cf017283233097b1ae1db3b00
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-04-01 09:21:06 +0530

    Update server.js

--- src/routes/server.js
+++ src/routes/server.js
@@ -97,54 +97,7 @@ app.post("/callback", (req, res) => {
       config.PaytmConfig.key,
       checksumhash
     );
-    console.log("Checksum Result => ", result, "\n");
-
-    // Send Server-to-Server request to verify Order Status
-    var params = { MID: config.PaytmConfig.mid, ORDERID: post_data.ORDERID };
-
-    checksum_lib.genchecksum(
-      params,
-      config.PaytmConfig.key,
-      function (err, checksum) {
-        params.CHECKSUMHASH = checksum;
-        post_data = "JsonData=" + JSON.stringify(params);
-
-        var options = {
-          hostname: "securegw-stage.paytm.in", // for staging
-          // hostname: 'securegw.paytm.in', // for production
-          port: 443,
-          path: "/merchant-status/getTxnStatus",
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Content-Length": post_data.length,
-          },
-        };
-
-        // Set up the request
-        var response = "";
-        var post_req = https.request(options, function (post_res) {
-          post_res.on("data", function (chunk) {
-            response += chunk;
-          });
-
-          post_res.on("end", function () {
-            console.log("S2S Response: ", response, "\n");
-
-            var _result = JSON.parse(response);
-            if (_result.STATUS == "TXN_SUCCESS") {
-              res.send("payment sucess");
-            } else {
-              res.send("payment failed");
-            }
-          });
-        });
-
-        // post the data
-        post_req.write(post_data);
-        post_req.end();
-      }
-    );
+    res.send(result);
   });
 });
 
